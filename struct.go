399bcf8f847047f0306af8ff0a80ae6c602d1fee
farkhanisturkia <farkhan22032000@gmail.com>
2025-10-01 16:12:58 +0700

    mamdani type with Declatarions type

--- struct.go
+++ struct.go
@@ -4,3 +4,13 @@ type Datas struct {
     Input      map[string]float64   `json:"Input"`
     Dataset    []map[string][]float64 `json:"Dataset"`
 }
+
+type Declaration struct {
+	Min      float64   `json:"Min"`
+	Mid      float64   `json:"Mid"`
+	Max      float64   `json:"Max"`
+}
+
+type Mamdani struct {
+	Declarations    []map[string]Declaration `json:"Declaration"`
+}
