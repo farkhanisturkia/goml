7d8d07336a11b23051a43c926e2e21f5d86d3eb2
farkhanisturkia <farkhan22032000@gmail.com>
2025-10-01 14:17:54 +0700

    change struct  param

--- struct.go
+++ struct.go
@@ -2,5 +2,5 @@ package goml
 
 type Datas struct {
     Input      map[string]float64   `json:"Input"`
-    Fuzzifikasi []map[string]map[string]float64 `json:"Fuzzifikasi"`
+    Fuzzification []map[string]map[string]float64 `json:"Fuzzification"`
 }
