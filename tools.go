4d40aa110d3c2e94f3ace5fca87be8129e0d5376
farkhanisturkia <farkhan22032000@gmail.com>
2025-10-02 11:48:10 +0700

    validationJSON with models parameter

--- tools.go
+++ tools.go
@@ -15,24 +15,31 @@ func ParseJSON(jsonInput []byte) (*Datas, error) {
     return &datas, nil
 }
 
-func ValidationJSON(d *Datas) error {
-    inputKeys := make([]string, 0, len(d.Input))
-    for key := range d.Input {
-        inputKeys = append(inputKeys, key)
-    }
-    sort.Strings(inputKeys)
-
-    if len(inputKeys) != len(d.Dataset) {
-        return fmt.Errorf("mismatch in Input and Dataset: input keys length (%d) != dataset entries (%d)", len(inputKeys), len(d.Dataset))
-    }
-
-    for i, dataset := range d.Dataset {
-        for datasetKey := range dataset {
-            if datasetKey != inputKeys[i] {
-                return fmt.Errorf("mismatch in Input and Dataset: input key %s != dataset key %s at index %d", inputKeys[i], datasetKey, i)
+func ValidationJSON(d *Datas, models Models) error {
+    switch models.Model {
+	case FMamdani:
+        inputKeys := make([]string, 0, len(d.Input))
+        for key := range d.Input {
+            inputKeys = append(inputKeys, key)
+        }
+        sort.Strings(inputKeys)
+    
+        if len(inputKeys) != len(d.Dataset) {
+            return fmt.Errorf("mismatch in Input and Dataset: input keys length (%d) != dataset entries (%d)", len(inputKeys), len(d.Dataset))
+        }
+    
+        for i, dataset := range d.Dataset {
+            for datasetKey := range dataset {
+                if datasetKey != inputKeys[i] {
+                    return fmt.Errorf("mismatch in Input and Dataset: input key %s != dataset key %s at index %d", inputKeys[i], datasetKey, i)
+                }
             }
         }
-    }
+    case FTsukamoto:
+		return fmt.Errorf("Tsukamoto validation not implemented yet")
+	default:
+		return fmt.Errorf("unknown model: %s", models.Model)
+	}
 
     return nil
 }
